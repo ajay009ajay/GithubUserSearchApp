5381313d54318b052e6d5a5c26850dadbca83564
user <user@users-MBP.domain.name>
2020-04-14 00:34:54 +0530

    first commit

---
+++ GithubUserSearchApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubUserSearchApp
+//
+//  Created by user on 4/14/20.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
